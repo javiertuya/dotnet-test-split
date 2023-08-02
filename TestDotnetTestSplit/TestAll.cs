679e1c7bb373ae03e1234d87b2f6a43d23b3c374
Javier <tuya@uniovi.es>
2023-08-02 11:42:39 +0200

    Fix XUnit 2.5.0 breaking change

--- TestDotnetTestSplit/TestAll.cs
+++ TestDotnetTestSplit/TestAll.cs
@@ -9,6 +9,17 @@ using Giis.DotnetTestSplit;
 
 namespace Test4Giis.DotnetTestSplit
 {
+    /// <summary>
+    /// Tests are intended to run from commandline, in a GitHub Acctions workflow or in local:
+    /// Execute commands in file run-test.bat that
+    /// - Generates the test assets for each platform by executing the tess (most of tests must fail)
+    /// - Optionally execute 'ant report' to see a junit style html report for each platform
+    /// - Compiles, installs and executes DotnetTestSplit
+    /// - Executes this test or each class and platform producing the result in a .trx file
+    ///   under the TestResults folder of this project
+    /// - As comparison is made with VisualAssert, the html files that were produced containt the detailed
+    ///   expected/actual test comparison
+    /// </summary>
     [TestFixture]
     public class TestSplit
     {
@@ -30,23 +41,15 @@ namespace Test4Giis.DotnetTestSplit
         [TestCase("Xunit", "Stp.ClassStp")]
         public void TestAll(string framework, string testClass)
         {
-            //Generation of trx files y split must be executed from outside
+            // Generation of trx files y split must be executed from outside
+            // Here we compare th splitted files for all combinations of class that produce the report and platform
+            // Uses VisualAssert to have a clear display of differences in tests that fail
             string testFileName = "TEST-TestAssets" + framework + "." + testClass + ".xml";
             string expectedFolder = "../../../../expected/" + framework.ToLower() + "-report.trx.split";
             string outFolder = "../../../../reports/" + framework.ToLower() + "-report.trx.split";
-            //uncomment/customize only for debug
-            //DotnetTestSplitMain w = new DotnetTestSplitMain();
-            //w.Run("../../../../reports/mstest-report.trx", outFolder);
-
             //remove times and sort expected and actual to make comparable
             string expected = ReadComparable(Path.Combine(expectedFolder, testFileName));
             string actual = ReadComparable(Path.Combine(outFolder, testFileName));
-
-            //to manually check differences of compared data
-            //Directory.CreateDirectory(Path.Combine(outFolder + "compare-expected"));
-            //Directory.CreateDirectory(Path.Combine(outFolder + "compare-actual"));
-            //File.WriteAllText(Path.Combine(outFolder + "compare-expected", testFileName), expected);
-            //File.WriteAllText(Path.Combine(outFolder + "compare-actual", testFileName), actual);
 
             VisualAssert va = new VisualAssert().SetUseLocalAbsolutePath(true); 
             va.AssertEquals(expected, actual, "Comparing file: " + testFileName, testFileName + ".html");
